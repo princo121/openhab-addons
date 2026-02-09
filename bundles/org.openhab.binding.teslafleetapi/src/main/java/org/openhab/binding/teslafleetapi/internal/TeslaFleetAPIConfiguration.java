2e12bc1c369786d58a02fc039792552c13b76d54
Mirko Valenti <valmirko@tiscali.it>
2026-02-09 12:17:33 +0100

    Update TeslaFleetAPIConfiguration.java
    Signed-off-by: Mirko Valenti <valmirko@tiscali.it>

--- bundles/org.openhab.binding.teslafleetapi/src/main/java/org/openhab/binding/teslafleetapi/internal/TeslaFleetAPIConfiguration.java
+++ bundles/org.openhab.binding.teslafleetapi/src/main/java/org/openhab/binding/teslafleetapi/internal/TeslaFleetAPIConfiguration.java
@@ -1,31 +1,58 @@
-/*
- * Copyright (c) 2010-2026 Contributors to the openHAB project
- *
- * See the NOTICE file(s) distributed with this work for additional
- * information.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0
- *
- * SPDX-License-Identifier: EPL-2.0
- */
-package org.openhab.binding.teslafleetapi.internal;
+package org.openhab.binding.teslafleet.internal;
 
-import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
 
 /**
- * The {@link TeslaFleetAPIConfiguration} class contains fields mapping thing configuration parameters.
+ * Configuration class for the Tesla Fleet API Bridge.
  *
- * @author Mirko Valenti - Initial contribution
+ * All fields map 1:1 to the configuration parameters of the Bridge Thing.
+ * These values are injected automatically by openHAB from the thing configuration.
+ *
+ * Typical fields:
+ *   - region:            API region ("na", "eu", "cn")
+ *   - clientId:          OAuth client id
+ *   - clientSecret:      OAuth client secret
+ *   - redirectUri:       Redirect URL used during authorization
+ *   - refreshToken:      OAuth refresh token (persisted once obtained)
+ *   - pollingIntervalSeconds: how often the bridge polls Tesla for sanity/online checks
+ *
+ * This class must be a pure POJO: getters/setters are not required.
  */
-@NonNullByDefault
-public class TeslaFleetAPIConfiguration {
+public class TeslaFleetBridgeConfiguration {
 
     /**
-     * Sample configuration parameters. Replace with your own.
+     * Region of the Tesla Fleet API cluster. Typical values:
+     *   - "na": North America
+     *   - "eu": Europe
+     *   - "cn": China
      */
-    public String hostname = "";
-    public String password = "";
-    public int refreshInterval = 600;
+    public @Nullable String region;
+
+    /**
+     * OAuth Client ID of the registered Tesla application.
+     */
+    public @Nullable String clientId;
+
+    /**
+     * OAuth Client Secret of the registered Tesla application.
+     */
+    public @Nullable String clientSecret;
+
+    /**
+     * Redirect URI configured in the Tesla OAuth application,
+     * used during the authorization flow.
+     */
+    public @Nullable String redirectUri;
+
+    /**
+     * Refresh token obtained after completing the OAuth authorization flow.
+     * Stored persistently once the user authorizes the bridge.
+     */
+    public @Nullable String refreshToken;
+
+    /**
+     * Polling interval for internal status checks (seconds).
+     * Default is 60 if not specified.
+     */
+    public int pollingIntervalSeconds = 60;
 }
